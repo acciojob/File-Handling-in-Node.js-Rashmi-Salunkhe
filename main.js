d6b91d4d602061a214dfbc5c57f024e591695a70
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-09 12:37:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
+const csvFilePath = process.argv[2];const fs = require('fs');
+const csv = require('csv-parser');
+
 const csvFilePath = process.argv[2];
+const columnName = process.argv[3];
+
+let sum = 0;
+
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on('data', (data) => {
+    // TODO: Add code to sum values in the specified column
+  })
+  .on('end', () => {
+    console.log(`The sum of ${columnName} is: ${sum}`);
+  });
 const columnName = process.argv[3];
 
 let sum = 0;
